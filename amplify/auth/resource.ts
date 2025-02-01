3185e81ae58545f1cf076cf3ede700fdca587b1a
Tristan Graaff <tristangraaff@yahoo.com>
2025-02-01 16:38:55 +0100

    set up amplify and added notes app with backend

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,24 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData, defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
 });
+
+// export const auth = defineAuth({
+//   userPool: {
+//     signInAliases: { username: true, email: true },
+//     autoVerifiedAttributes: ['email'],
+//     passwordPolicy: {
+//       minLength: 8,
+//       requireLowercase: true,
+//       requireUppercase: true,
+//       requireNumbers: true,
+//       requireSymbols: true,
+//     },
+//   },
+//   identityPool: {
+//     allowUnauthenticatedIdentities: false,
+//   },
+// });
